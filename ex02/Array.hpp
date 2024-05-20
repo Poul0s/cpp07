02c029cc25132f75e535a7c9f3cc0258aa25959c
Paul Salame <psalame@student.42angouleme.fr>
2024-05-20 12:54:27 +0200

    added Array.tpp

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: psalame <psalame@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 15:30:02 by psalame           #+#    #+#             */
-/*   Updated: 2024/04/29 17:00:06 by psalame          ###   ########.fr       */
+/*   Updated: 2024/04/29 18:23:39 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,73 +33,6 @@ class Array {
 		T				*_arr;
 };
 
-
-// constructors definitions
-
-template<class T>
-Array<T>::Array(void)
-{
-	this->_size = 0;
-	this->_arr = NULL;
-}
-
-template<class T>
-Array<T>::Array(unsigned int size)
-{
-	this->_size = size;
-	this->_arr = new T[size];
-	if (!this->_arr)
-		throw std::exception();
-}
-
-template<class T>
-Array<T>::Array(const Array &copy)
-{
-	this->_arr = NULL;
-	*this = copy;
-}
-
-template<class T>
-Array<T>::~Array(void)
-{
-	if (this->_arr)
-		delete[] this->_arr;
-}
-
-
-// Operator overload
-
-template<class T>
-Array<T>	&Array<T>::operator=(const Array &copy)
-{
-	if (this->_arr != NULL)
-		delete[] this->_arr;
-
-	this->_size = copy._size;
-	this->_arr = new T[copy._size];
-	if (!this->_arr)
-		throw std::exception();
-	for (unsigned int i = 0; i < this->_size; i++)
-		this->_arr[i] = copy._arr[i];
-
-	return (*this);
-}
-
-template<class T>
-T		&Array<T>::operator[](unsigned int i)
-{
-	if (i >= this->_size)
-		throw std::exception();
-	return (this->_arr[i]);
-}
-
-
-// member function
-
-template<class T>
-unsigned int	Array<T>::size(void)
-{
-	return this->_size;
-}
+#include "Array.tpp"
 
 #endif
